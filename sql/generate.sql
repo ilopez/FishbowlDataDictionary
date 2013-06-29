fd3395b03491bdc176a4bfa3b9538e311e2f3de6
Israel Lopez <israel@ilopez.com>
2013-06-29 09:14:40 -0700

    Added template for column and table commenting

---
+++ sql/generate.sql
@@ -0,0 +1,17 @@
+/* COMMENT ON TABLE ACCOUNTGROUP IS 'Test' */
+
+
+/* Following SQL adapted from http://www.firebirdfaq.org/faq174/ */
+select rdb$relation_name, 'COMMENT ON TABLE '||TRIM(rdb$relation_name)||' IS ''X'' ; ' 
+from rdb$relations
+where rdb$view_blr is null 
+and (rdb$system_flag is null or rdb$system_flag = 0)
+order by 1 asc
+
+/* COMMENT ON COLUMN ACCOUNTGROUP.ID IS 'Blah blah TEST' */
+select f.rdb$relation_name, f.rdb$field_name, 'COMMENT ON COLUMN'||TRIM(f.rdb$relation_name)||'.'||TRIM(f.rdb$field_name)||' ID ''X'' ; ' as cmt
+from rdb$relation_fields f
+join rdb$relations r on f.rdb$relation_name = r.rdb$relation_name
+and r.rdb$view_blr is null 
+and (r.rdb$system_flag is null or r.rdb$system_flag = 0)
+order by 1, f.rdb$field_position
